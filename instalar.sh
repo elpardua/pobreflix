edc6a16e8a0b073466f62b4927717a8a2b29c78c
Pablo Arduino <38364483+elpardua@users.noreply.github.com>
2022-03-06 17:41:51 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-mkdir -p /docker/dockervolumes
-mkdir /docker/composefiles
-mkdir /docker/pobreflix
-mkdir /Downloads
+#Armo toda la estructura de directorios
+sudo mkdir -p /docker/dockervolumes
+sudo mkdir /docker/composefiles
+sudo mkdir /docker/pobreflix
+sudo mkdir /Downloads
+sudo chown -R $USER:$USER /Downloads
+sudo chown -R $USER:$USER /docker
 
 echo "Bienvenido al instalador de pobreflix, un combo loco de alegror automatizado... Banque un toque."
 sleep 3
 echo ""
+
 echo "Voy a instalar los requerimientos."
 echo ""
-apt update && apt install docker.io docker-compose -y
+sudo apt update && sudo apt install docker.io docker-compose -y
+
 echo "Activando el daemon automáticamente en el arranque"
 echo ""
 systemctl enable docker --now
+
 echo "Agregando tu usuario al grupo docker para que pueda correr contenedores"
 echo ""
-usermod -a -G docker $USER
+sudo usermod -a -G docker $USER
 mv docker-compose.yaml.template /docker/composefiles/docker-compose.yaml.template
+
 echo "Te creaste ya una cuenta en plex.tv? Si no lo hiciste te espero. Dale enter si ya está hecho"
 read
+
 echo "Ahora necesito que entres en https://www.plex.tv/claim, copies el código al toque y lo pegues acá, Recordá que expira a los 4 minutos de generado, no te duermas...:"
 read plex_claim
+
 echo "Modificando config"
 echo ""
+
 cd /docker/composefiles/ && sed -i 's/CAMBIAME-CARAJO/$plex_claim/g' docker-compose.yaml.template > /docker/composefiles/docker-compose.yaml && rm -f docker-compose.yaml.template
+
 echo "Inicializando los contenedores, a cruzar lo' dedo'..."
 cd /docker/composefiles && docker-compose up -d && echo "Lista de contenedores activos" && docker ps
